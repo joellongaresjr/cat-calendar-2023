0d224cfcb36acf3b1a5599b56a64a860c43d747c
Joel <joellongaresjr@gmail.com>
2023-07-11 20:36:44 -0700

    finalized all code , keep forgetting to save files and then pushing the commit

--- Assets/script.js
+++ Assets/script.js
@@ -1,37 +1,32 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
 
+  // This code attaches a click event handler to elements with the class "saveBtn"
+  // When a save button is clicked, the function inside the handler is executed.
   $(".saveBtn").on("click", function () {
+  // The line below retrieves the value of the input field with the class "description" that is a sibling of the clicked save button.
+  // thus assigns the value to the variable "description"
     var description = $(this).siblings(".description").val();
     var hourId = $(this).parent().attr("id");
     console.log("description", description, "hourId", hourId);
-    //create two var 1. holds the id of the timeblock 2.description of what was saved
-    // key = id value = description
-    // save info to local storage
     localStorage.setItem(hourId, description);
   });
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+
   function colorHour() {
     var currentHour = dayjs().hour();
-    // console.log(currentHour, "currentHour")
     $(".time-block").each(function(){
+    // (very pleased with my understanding with the line below! shoutout to my tutor and Kyle:)
+    // the line below retrieves the id attribute of the current time block element, splits it using the "-" character as the seperator,
+    // and gets the second element from the resulting array. It then assigns the value to the 'timeBlock' variable 
       var timeBlock = $(this).attr("id").split("-")[1]
+    // this line below converts the 'timeBlock' string to an inter using the 'parseInt()' function and assigns the value to the 'timeBlockHour' variable.
       var timeBlockHour = parseInt(timeBlock)
-      // console.log(timeBlockHour, "timeBlockHour")
+    //removes CSS classes from the current time block element  
       $(this).removeClass("past present future")
+    // If-else statements that follow compare 'timeBlockHour' with 'currentHour' and add the appropriate CSS class
+    // to the current current time block element based on the comparison.
+    // If 'timeBlockhour' is less than the 'currentHour', the "past" class is added. 
+    // If they are equal, the "present" class is added. Otherwise, the "future" class is added.
       if (timeBlockHour < currentHour) {
         $(this).addClass("past");
       } else if (timeBlockHour === currentHour) {
@@ -40,111 +35,33 @@ $(document).ready(function () {
         $(this).addClass("future");
       }
     })
-  
-
-   
-    // set interval for function colorhour and setting a time 
-    // handling removing classes
-    // for (var i = 9; i <= 17; i++) var timeBlockEl = $("#hour-" + i);
   }
+  // $(".time-block").each()
+  // for loop that loops over each timeblock and uns our if/else statement
+  // create a function that will call
+  //create a var that holds the current time
+  // select all timeblocks and loop over them
+  // thus compare it with the current hour (conditional statements if time block < current hour past) (current hour > === future)
 
-  // // $(".time-block").each()
-  // // for loop that loops over each timeblock and uns our if/else statement
-  //   // create a function that will call
-  //   //create a var that holds the current time
-  //   // select all timeblocks and loop over them
-  //   // thus compare it with the current hour (conditional statements if time block < current hour past) (current hour > === future)
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-
+// This code below selects all elements with the class "description" and iterates over each element using the 'each()'function  
 $(".description").each(function() {
+// This line below gets the id of the parent element of the current descripition element and assigns it to the 'timeBlock' variable
   var timeBlock = $(this).parent().attr("id");
+// The line below retrieves the data stored in the localStorage object using 'timeBlock' as they key. It assigns the retrieved value to the 'savedData' variable.
   var savedData = localStorage.getItem(timeBlock);
+ // the if statements check if 'savedData' exists thus it sets the value of the current description element to the retrieved data using the 'val()' function
+ // 
   if (savedData) {
     $(this).val(savedData);
   }
 })
 
-  // // // add code to get any users inputs that was saved in local storage
-  // var nineAm = localStorage.getItem("hour-9");
-  // $("#description9").val(nineAm);
-
-  // var tenAm = localStorage.getItem("hour-10");
-  // $("#description10").val(tenAm);
-
-  // var elevenAm = localstorage.getItem("hour-11");
-  // $("#descripition11").val(elevenAm);
-
-  // var twelvePm = localstorage.getItem("hour-12");
-  // $("#descripition12").val(twelvePm);
-
-  // var onePm = localstorage.getItem("hour-13");
-  // $("#descripition13").val(onePm);
-
-  // var twoPm = localstorage.getItem("hour-14");
-  // $("#descripition14").val(twoPm);
-
-  // var threePm = localstorage.getItem("hour-15");
-  // $("#descripition15").val(threePm);
-
-  // var fourPm = localstorage.getItem("hour-16");
-  // $("#descripition16").val(fourPm);
-
-  // var fivePm = localstorage.getItem("hour-17");
-  // $("#descripition17").val(fivePm);
-
-  //conditional statements
-  //set intervals 
-  //
-  // create for loop all different timeblocks (local.stroage.getitem)
-  // if item is not null then set textcontent to whatever current content it is (conditionally statement)
-
-  // TODO: Add code to display the current date in the header of the page.[]
+// code below represents the use of dayjs library to get the current date and formatting it to the variable that is "currentDate"
   var currentDate = dayjs().format("MM-DD-YYYY");
   $("#currentDay").text(currentDate);
 
+// calling my function colorHour()  
   colorHour();
-  setInterval(colorHour, 6000)
+// code sets up the interval that call colorHour function every 60 seconds to update the color of the time blocks based on the current hour.
+  setInterval(colorHour, 60000)
 });
-
-// // What is needed
-
-// // code for past present future timeblocks + color coordinate
-// var currentHour = dayjs().format("HH");
-// for (var i = 0; i < 18; i++)
-// var timeBlockEl = $("#hour-" + i)
-// if // past
-//   (if current hour > i )
-// else if // present
-// else // future
-// // $(".time-block").each()
-// // for loop that loops over each timeblock and uns our if/else statement
-//   // create a function that will call
-//   //create a var that holds the current time
-//   // select all timeblocks and loop over them
-//   // thus compare it with the current hour (conditional statements if time block < current hour past) (current hour > === future)
-
-// // add code to get any users inputs that was saved in local storage
-// var nineAm = localStorage.getItem("9") // repeat for each hour block
-// document.getElementById("9").value=nineAm
-// $("#9").val(nineAm)
-
-// var tenAm = localStorage.getItem("10")
-// $("#10").val(tenAm)
-//   // create for loop all different timeblocks (local.stroage.getitem)
-//   // if item is not null then set textcontent to whatever current content it is (conditionally statement)
-// // add event listener for the save button
-// // jquery select class class="btn saveBtn col-2 col-md-1"
-// $(".saveBtn").on("click", function()){
-//   // when any savebutton is clicked, run this function
-//   // select the text area that is next to this particular save button
-// }
-//   // create a function that handles the event
-//   //create two var 1. holds the id of the timeblock 2.description of what was saved
-//   // key = id value = description
-//   // save info to local storage
-// // set interval check time throughout the day ( to check if update if needed )
-//   // set interval dom method recall functions , set time on it (past, present, future for call back function)
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # cat-calendar-2023
+## Description 
+
+Meow! Welcome to Cat Calendar 2023
+In this application, you will find a simple work day scheduler. You're able to input text that will save in certain timeblocks. Those timeblocks are updated real time, to help you the user, to feel organized on a regular work day schedule. Each timeblock is color coordinated to allow the user to know your schedule in real time. The timeblocks consists of time slots of a 9am-5pm work schedule. If your schedule runs on this kind of format, then this application is right for you! 
+
+## Technology Used 
+
+DayJs
+Jquery
+
+## Installation
+
+Here is a preview of what the application should look like!
+[Screenshot](Assets/Images/Screenshot%202023-07-11%20195832.png)
+
+Follow the link below to get directed to the website. 
+[Link](https://joellongaresjr.github.io/cat-calendar-2023/)
+
+
+## Usage 
+One thing to note! The application will know the current time of the User. So if it's passed the set time, the block will be Gray. The current hour will represent the color red in the respected timeblock. Future timeblocks that haven't but will occur will be highligted in green. 
+
+
+## Credits
+Shout out Kyle Vance (TA) & Julio Valdez (my go to tutor)
+Appreciate the support and believing in my abilities to bounce back in the class!
+
+
+
+
+
